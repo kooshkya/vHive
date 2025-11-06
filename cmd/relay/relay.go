199b211dce4e3a029b488a8b557f89ddaa2c4a0c
Amir Kooshky <kooshkya@gmail.com>
2025-11-07 01:51:24 +0330

    bug fix to previous commit (also needs to be reverted)

--- cmd/relay/relay.go
+++ cmd/relay/relay.go
@@ -176,7 +176,7 @@ func main() {
 	clonePrefix := flag.String("clonePrefix", "172.18", "Prefix for node-accessible IP addresses of uVMs, expected subnet is /16")
 	dockerCredentials := flag.String("dockerCredentials", "", "Docker credentials for pulling images from inside a microVM") // https://github.com/firecracker-microvm/firecracker-containerd/blob/main/docker-credential-mmds
 	minioCredentials := flag.String("minioCredentials", "", "Minio credentials for uploading/downloading remote firecracker snapshots. Format: <minioAddr>;<minioAccessKey>;<minioSecretKey>")
-	memFileOptimizationMode = flag.Bool("memOpt", false, "Optimize the download and upload of MemoryFile in snapshots")
+	memFileOptimizationMode := flag.Bool("memOpt", false, "Optimize the download and upload of MemoryFile in snapshots")
 	flag.Parse()
 
 	minioAddr := "localhost:9000"
